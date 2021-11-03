76783120b6eb64ed3a5de79f04107706b07bf371
Kevin Smith <kevin.smith@coinbase.com>
2021-11-03 16:11:06 -0400

    Fix and separate fallback and receive functions

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -41,12 +41,11 @@ contract SimpleBank {
     /* Functions
      */
 
-    // Fallback function - Called if other functions don't match call or
-    // sent ether without data
-    // Typically, called when invalid data is sent
-    // Added so ether sent to this contract is reverted if the contract fails
-    // otherwise, the sender's money is transferred to contract
-    function () external payable {
+    fallback() external payable {
+        revert();
+    }
+
+    receive() external payable {
         revert();
     }
 
